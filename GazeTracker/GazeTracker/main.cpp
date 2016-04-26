17562e5bd10eb22ad17cec35041ddc1c63f8f0a4
Bernhard Rieder <bernhard.rieder@live.at>
2016-04-26 14:14:46 +0200

    created main example and precompiled header

---
+++ GazeTracker/GazeTracker/main.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cout << " Usage: display_image ImageToLoadAndDisplay" << std::endl;
+		return -1;
+	}
+	cv::Mat image;
+	image = cv::imread(argv[1], cv::IMREAD_COLOR); // Read the file
+	if (image.empty()) // Check for invalid input
+	{
+		std::cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	cv::namedWindow("Display window", cv::WINDOW_AUTOSIZE); // Create a window for display.
+	imshow("Display window", image); // Show our image inside it.
+	cv::waitKey(0); // Wait for a keystroke in the window
+	return 0;
+}
