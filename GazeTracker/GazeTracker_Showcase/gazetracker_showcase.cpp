ead4257c558d7baaf6286dfc417b36e0889a12c3
Bernhard Rieder <bernhard.rieder@live.at>
2016-06-14 00:14:55 +0200

    created showcase project

---
+++ GazeTracker/GazeTracker_Showcase/gazetracker_showcase.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "gazetracker_showcase.h"
+
+GazeTracker_Showcase::GazeTracker_Showcase(QWidget *parent)
+	: QWidget(parent)
+{
+	ui.setupUi(this);
+}
+
+GazeTracker_Showcase::~GazeTracker_Showcase()
+{
+
+}
